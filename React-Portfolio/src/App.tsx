b127bee2ac90fdedd5e270f22390fb65eb89ff6e
kmmoody212 <kmfoster212@gmail.com>
2024-12-02 17:57:51 -0500

    updated css files, added files: Navbar, router, Contact, and Dashboard.

--- React-Portfolio/src/App.tsx
+++ React-Portfolio/src/App.tsx
@@ -1,12 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-   
+    <>
+      <Navbar />
+      <h1>Home page</h1>
+    </>
   );
 }
 
--- React-Portfolio/src/main.tsx
+++ React-Portfolio/src/main.tsx
@@ -1,14 +1,14 @@
-// Using bootcamp activity boilerplate
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
